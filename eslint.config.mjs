abfb8b04d0ebe5c8a15b30fd242d5929f7ad2522
lefline <issamkharbache2@gmail.com>
2025-02-06 20:35:07 +0100

    editing the eslint file to ignore the no used var error starting with __

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    overrides: [
+      {
+        files: ["*.ts", "*.tsx"],
+        rules: {
+          "@typescript-eslint/no-unused-vars": [
+            "warn",
+            {
+              argsIgnorePattern: "^__",
+              varsIgnorePattern: "^__",
+            },
+          ],
+        },
+      },
+    ],
+  },
 ];
 
 export default eslintConfig;
