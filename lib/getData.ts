f81171372b10e6f3d0170891e00c20892019e83f
lefline <issamkharbache2@gmail.com>
2025-03-21 15:38:44 +0000

    feat: add cache control headers to API requests in getData function

--- lib/getData.ts
+++ lib/getData.ts
@@ -7,7 +7,12 @@ export const getData = async (endpoint: string) => {
     if (!baseUrl) {
       throw new Error("NEXT_PUBLIC_BASE_URL is not defined");
     }
-    const response = await axios.get(`${baseUrl}${endpoint}`);
+    const response = await axios.get(`${baseUrl}${endpoint}`, {
+      headers: {
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+      },
+    });
     return response.data.data;
   } catch (error) {
     console.error("Error in getData:", error);
